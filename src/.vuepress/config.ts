509effe8e16f3c857d93dedb084a42b9efc29559
18848782090@139.com <18848782090@139.com>
2025-02-08 20:32:29 +0800

    修改模板

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,9 +4,8 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/blog/",
-
   lang: "zh-CN",
-  title: "博客演示",
+  title: "Alex 的博客",
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
